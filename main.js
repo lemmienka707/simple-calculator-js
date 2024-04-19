a923674dab1261676dcff17837ca26eea4d02063
Kaylee Lemmien <lemmienka707@northwested.org>
2024-04-19 10:11:17 -0400

    simple calculator

---
+++ main.js
@@ -0,0 +1,23 @@
+// Kaylee Lemmien
+// 4/19/24
+// Simple Calculator
+
+// Input
+const firstNum = Number(prompt('Enter first number:'));
+const secondNum = Number(prompt('Enter second number:'));
+
+// Process
+const add = firstNum + secondNum;
+const subtract = firstNum - secondNum;
+const multiply = firstNum * secondNum;
+const divide = firstNum / secondNum;
+
+// Output
+console.log('Kaylee Lemmien')
+console.log('4/19/24')
+console.log('JS Simple Calculator')
+
+console.log(`${firstNum} + ${secondNum} = ${add}`)
+console.log(`${firstNum} - ${secondNum} = ${subtract}`)
+console.log(`${firstNum} * ${secondNum} = ${multiply}`)
+console.log(`${firstNum} / ${secondNum} = ${divide}`)
